6e4b9f624ca2d06f8bea00229a05308986dfdd45
gtraiano <gtraiano@gmail.com>
2020-12-05 00:21:35 +0200

    - About: switched to 2 column layout - Added translations

--- frontend/src/components/About.vue
+++ frontend/src/components/About.vue
@@ -11,80 +11,110 @@
 
 <template>
 <b-container fluid>
-	<div>
-		<h1>{{ $t('about') }}</h1>
-		<p> 
-			<div style="text-align: left" v-html="$t('about info')"></div>
-		</p>
-	</div>
-	<div>
-		<h1>{{$t('functions')}}</h1>
-		<div class="text-left">
-			<h2>{{$t('search-add.header')}}</h2>
-			<li>
-				<ul>
-					{{$t('search-add.step1')}}
-				</ul>
-				<ul>
-					<div style="display: inline-block;">
-						<b-input-group>
-						    	<b-form-input
-						    		class="search"
-						    		:placeholder="$t('add city')"
-						    	/>
-						    	<template #append>
-						      		<b-button
-						      			:title="$t('search forecast data')"
-						      		>
-						      			<b-icon-search/>
-						      		</b-button>
-						    	</template>
-						  	</b-input-group>
-					</div>
-				</ul>
-
-				<ul>
-					{{$t('search-add.step2')}}
-				</ul>
-				<ul>
-					<SearchResults
-						:results="[
-							{ lat: 0, lon: 0, name: $t('city name'), region: $t('region'), postCode: $t('postal code'), country: $t('country') },
-							{ lat: 1, lon: 1, name: 'Another City', region: 'Nowhere', postCode: '123456', country: 'Noland' }
-						]" 
-						:onClick="() => {}"
-					/>
-				</ul>
-				<ul id="promptlist">
-					{{$t('search-add.step3')}}
-				</ul>
-				<ul>
-					{{$t('search-add.step4')}}
-				</ul>
-				<ul>
-					<SearchResults
-						:results="[
-							{ lat: 0, lon: 0, name: $t('city name'), region: $t('region'), postCode: $t('postal code'), country: $t('country') },
-							{ lat: 1, lon: 1, name: 'Another City', region: 'Nowhere', postCode: '123456', country: 'Noland' }
-						]" 
-						:onClick="() => {}"
-						:alreadyAdded="[{ lat: 0, lon: 0 }]"
-						:exists="[{ lat: 1, lon: 1 }]"
-					/>
-				</ul>
-			</li>
-		</div>
-
-		<div class="text-left mt-5">
-			<h2>{{$t('refresh forecast')}}</h2>
-			<li>
-				<ul>
-					{{$t('click on')}} <b-icon-arrow-clockwise class="ml-1 mr-1" scale="1.5" /> {{$t('refetch data')}}
-				</ul>
-			</li>
-		</div>
+	<b-row>
+		<b-col/>
 		
-		<div class="text-left mt-5">
+		<b-col cols="10">
+			<h1>{{ $t('about') }}</h1>
+		</b-col>
+
+		<b-col/>
+	</b-row>
+	
+	<b-row>
+		<b-col />
+		
+		<b-col cols="10">
+			<div style="text-align: left" v-html="$t('about info')"></div>
+		</b-col>
+
+		<b-col />
+	</b-row>
+	
+	<b-row>
+		<b-col/>
+		
+		<b-col cols="10">
+			<h1>{{$t('functions')}}</h1>
+		</b-col>
+
+		<b-col/>
+	</b-row>
+
+	<b-row/>
+	
+	<b-row>
+		<b-col/>
+
+		<b-col cols="5">
+			<div class="text-left">
+				<h2>{{$t('search-add.header')}}</h2>
+				<li>
+					<ul>
+						{{$t('search-add.step1')}}
+					</ul>
+					<ul>
+						<div style="display: inline-block;">
+							<b-input-group>
+							    	<b-form-input
+							    		class="search"
+							    		:placeholder="$t('add city')"
+							    	/>
+							    	<template #append>
+							      		<b-button
+							      			:title="$t('search forecast data')"
+							      		>
+							      			<b-icon-search/>
+							      		</b-button>
+							    	</template>
+							  	</b-input-group>
+						</div>
+					</ul>
+
+					<ul>
+						{{$t('search-add.step2')}}
+					</ul>
+					<ul>
+						<SearchResults
+							:results="[
+								{ lat: 0, lon: 0, name: $t('city name'), region: $t('region'), postCode: $t('postal code'), country: $t('country') },
+								{ lat: 1, lon: 1, name: 'Another City', region: 'Nowhere', postCode: '123456', country: 'Noland' }
+							]" 
+							:onClick="() => {}"
+						/>
+					</ul>
+					<ul id="promptlist">
+						{{$t('search-add.step3')}}
+					</ul>
+					<ul>
+						{{$t('search-add.step4')}}
+					</ul>
+					<ul>
+						<SearchResults
+							:results="[
+								{ lat: 0, lon: 0, name: $t('city name'), region: $t('region'), postCode: $t('postal code'), country: $t('country') },
+								{ lat: 1, lon: 1, name: 'Another City', region: 'Nowhere', postCode: '123456', country: 'Noland' }
+							]" 
+							:onClick="() => {}"
+							:alreadyAdded="[{ lat: 0, lon: 0 }]"
+							:exists="[{ lat: 1, lon: 1 }]"
+						/>
+					</ul>
+				</li>
+			</div>
+
+			<div class="text-left mt-5">
+				<h2>{{$t('refresh forecast')}}</h2>
+				<li>
+					<ul>
+						{{$t('click on')}} <b-icon-arrow-clockwise class="ml-1 mr-1" scale="1.5" /> {{$t('refetch data')}}
+					</ul>
+				</li>
+			</div>
+		</b-col>
+
+		<b-col cols="5">
+			<div class="text-left">
 			<h2>{{ $t('forecasts') }}</h2>
 			<!--h5>{{ $t('forecasts.header2', [$t('actions'), $t('overview')]) }}</h5-->
 			<li>
@@ -206,7 +236,10 @@
 				</ul>
 			</li>
 		</div>
-	</div>
+		</b-col>
+
+		<b-col/>
+	</b-row>
 </b-container>
 </template>
 
