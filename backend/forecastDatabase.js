840586d5da278b6d09257ed77b1f46e8903a1b32
gtraiano <gtraiano@gmail.com>
2020-12-07 22:18:36 +0200

    - removed try-catch from all functions - minor changes to connect() and close()

--- backend/forecastDatabase.js
+++ backend/forecastDatabase.js
@@ -3,102 +3,58 @@ const { MongoClient } = require('mongodb');
 const dbName = 'weather-forecast';
 const collectionName = 'forecasts';
 const uri = `mongodb://localhost:${process.env.MONGODB_PORT}`;
-const client = new MongoClient(uri, { useUnifiedTopology: true });
+let client = null;
 let database = null;
 let collection = null;
 
 
 const insertCity = async city => {
-	try {
-		const result = await collection.updateOne({ lat: Number.parseFloat(city.lat), lon: Number.parseFloat(city.lon) }, { $set: { ...city } }, { upsert: true });
-		return result;
-	}
-	catch(error) {
-		throw error;
-	}
+	const result = await collection.updateOne({ lat: Number.parseFloat(city.lat), lon: Number.parseFloat(city.lon) }, { $set: { ...city } }, { upsert: true });
+	return result;
 }
 
 const insertCities = async cities => {
-	try {
-		const result = await Promise.all( cities.map(async city => insertCity(city)) );
-		return result;
-	}
-	catch(error) {
-		throw error;
-	}
+	const result = await Promise.all( cities.map(async city => insertCity(city)) );
+	return result;
 }
 
 const updateCity = async (lat, lon, data) => {
-	try {
-		//const found = await collection.findOne({ lat: Number.parseFloat(lat), lon: Number.parseFloat(lon) });
-		const result = await collection.updateOne({ lat: Number.parseFloat(lat), lon: Number.parseFloat(lon) }, { $set: { ...data } });
-		return result;
-	}
-	catch(error) {
-		throw error;
-	}
+	const result = await collection.updateOne({ lat: Number.parseFloat(lat), lon: Number.parseFloat(lon) }, { $set: { ...data } });
+	return result;
 }
 
 const findCity = async (lat, lon) => {
-	try {
-		const result = await collection.findOne({ lat: Number.parseFloat(lat), lon: Number.parseFloat(lon) });
-		return result;
-	}
-	catch(error) {
-		throw error;
-	}
+	const result = await collection.findOne({ lat: Number.parseFloat(lat), lon: Number.parseFloat(lon) });
+	return result;
 }
 
 const allCities = async () => {
-	try {
-		const result = await collection.find({});
-		return result.toArray();
-	}
-	catch(error) {
-		throw error;
-	}
+	const result = await collection.find({});
+	return result.toArray();
 }
 
 const removeCity = async (lat, lon) => {
-	try {
-		const result = await collection.deleteOne({ lat: Number.parseFloat(lat), lon: Number.parseFloat(lon) });
-		return result;
-	}
-	catch(error) {
-		throw error;
-	}
+	const result = await collection.deleteOne({ lat: Number.parseFloat(lat), lon: Number.parseFloat(lon) });
+	return result;
 }
 
 const clearDatabase = async () => {
-	try {
-		await collection.deleteMany({});
-	}
-	catch(error) {
-		throw error;
-	}
+	await collection.deleteMany({});
 }
 
 const connect = async () => {
-	try {
-		await client.connect();
-		database = await client.db(dbName);
-		console.log("Connected successfully to MongoDB server on port", process.env.MONGODB_PORT);
-		collection = await database.collection(collectionName);
-	}
-	catch(error) {
-		throw error;
-	}
+	client = await MongoClient.connect(uri, { useUnifiedTopology: true });
+	database = await client.db(dbName);
+	collection = await database.collection(collectionName);
 }
 
 const close = async () => {
-	try {
-		await client.close();
-		console.log('Database connection closed');
-	}
-	catch(error) {
-		console.log(error.message);
-		throw error;
-	}
+	if(!client) return;
+	
+	await client.close();
+	client - null;
+	database = null;
+	collection = null;
 }
 
 module.exports = { 
