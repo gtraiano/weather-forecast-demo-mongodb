69cce458c6b0bbdad7bbcd0a2063c5e0603b34cf
gtraiano <gtraiano@gmail.com>
2020-12-18 16:02:38 +0200

    made dev config read .env from project root folder

--- frontend/config/webpack.dev.config.js
+++ frontend/config/webpack.dev.config.js
@@ -1,12 +1,21 @@
 const path = require('path');
+const fs = require('fs');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const webpack = require('webpack');
-const Dotenv = require('dotenv-webpack');
+const DotEnvWebpack = require('dotenv-webpack');
+
+// make .env variables available inside this config file and add them to process.env
+const envConfig = require('dotenv').config({ path: path.resolve(__dirname, '../../.env') }).parsed;
+for (let k in envConfig) {
+    process.env[k] = envConfig[k];
+}
 
 const HOST = process.env.HOST || '0.0.0.0';
 const PORT = process.env.PORT || '8080';
+const HTTPS = Number.parseInt(process.env.WEBPACK_DEV_SERVER_HTTPS) == true;
+
 
 const config = {
   mode: 'development',
@@ -78,19 +87,24 @@ const config = {
     hot: true,
     overlay: true,
     open: true,
-    openPage: 'http://localhost:8080/'
+    openPage: `${HTTPS ? 'https' : 'http'}://localhost:${PORT}/`,
+    ...HTTPS && { https: true },
+    // use credentials if provided
+    ...process.env.WEBPACK_DEV_SERVER_PRIVATE_KEY && { key: fs.readFileSync(path.resolve(process.env.WEBPACK_DEV_SERVER_PRIVATE_KEY)) },
+    ...process.env.WEBPACK_DEV_SERVER_CERTIFICATE && { cert: fs.readFileSync(path.resolve(process.env.WEBPACK_DEV_SERVER_CERTIFICATE)) },
+    ...process.env.WEBPACK_DEV_SERVER_CA && { ca: fs.readFileSync(path.resolve(process.env.WEBPACK_DEV_SERVER_CA)) }
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
 	    entry: 'index.html',
       template: 'index.html',
-      title: 'demo web forecast',
+      title: 'Weather Forecast Demo',
       inject: 'body'
     }),
 	  new MiniCssExtractPlugin({filename:"main.css"}),
 	  new VueLoaderPlugin(),
-    new Dotenv()
+    new DotEnvWebpack({ path: path.resolve(__dirname, '../../.env') })
   ],
   devtool: '#eval-source-map'
 };
