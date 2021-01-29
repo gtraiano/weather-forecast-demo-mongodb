4dfea917704ae8d47e7a814057dc1bdc7fdb725b
gtraiano <gtraiano@gmail.com>
2021-01-29 12:22:59 +0200

    - added scss loader - corrected output.publicPath - corrected .env path

--- frontend/config/webpack.prod.config.js
+++ frontend/config/webpack.prod.config.js
@@ -15,7 +15,7 @@ const config = {
   output: {
     path: path.resolve(__dirname, '../dist'),
     filename: 'build.js',
-    publicPath: '/'
+    publicPath: './'
   },
   resolve: {
 	alias: {
@@ -53,16 +53,27 @@ const config = {
 		  'css-loader',
 	    ]
 	  },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/,
-        use: [{
-          loader: 'file-loader',
-          options: {
-			esModule: false,
-            name: '[name].[ext]?[hash]'
-          }
-        }]
-      },
+    {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+    },
+    {
+      test: /\.(png|jpe?g|gif|svg)$/,
+      use: [{
+        loader: 'file-loader',
+        options: {
+		      esModule: false,
+          name: '[name].[ext]?[hash]'
+        }
+      }]
+    },
 	  {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
@@ -97,7 +108,7 @@ const config = {
         NODE_ENV: '"production"'
       }
     }),
-    new Dotenv(),
+    new Dotenv({ path: path.resolve(__dirname, '../../.env') }),
     new webpack.LoaderOptionsPlugin({
       minimize: true
     })
