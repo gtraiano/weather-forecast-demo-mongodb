ce96ed0772f69d1cff54eaf9431123d3f7274daa
gtraiano <gtraiano@gmail.com>
2022-10-16 13:08:28 +0300

    added missing end() in response

--- backend/routes/ping.js
+++ backend/routes/ping.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const pingRouter = Router();
 
 pingRouter.get('/ping', (req, res) => {
-    res.status(200).set('Content-Type', 'text/plain').send('pong');
+    res.status(200).set('Content-Type', 'text/plain').send('pong').end();
 });
 
 export default pingRouter;
